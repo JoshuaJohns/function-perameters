b6a14ad1270a1e8f7a3304d0191b55da89e05e76
Joshua Johns <joshuajohns28@outlook.com>
2022-10-07 10:40:04 -0500

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+const quarterValue = (originalNumber) => {
+        let newNumber = originalNumber / 4
+    return newNumber
+}
+
+// Invoke the function and store the return value
+const result = quarterValue(16)
+
+// Log the return value
+console.log(result)
